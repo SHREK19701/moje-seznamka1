c6dff1b77de72dd91af3bbe138423127ecb9bf21
SHREK19701 <jardaolaholah@gmail.com>
2024-12-04 16:46:07 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const io = socketIo(server);
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'localStorage',
+    database: 'DB_URL',
     password: 'Charalamba11@',
     port: 5432,
 });
@@ -55,11 +55,11 @@ io.on('connection', (socket) => {
 // Stránky a routy
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/registrace', (req, res) => res.sendFile(path.join(__dirname, 'registrace.html')));
-app.get('/prihlaseni', (req, res) => res.sendFile(path.join(__dirname, 'přihlášení.html')));
+app.get('/prihlaseni', (req, res) => res.sendFile(path.join(__dirname, 'prihlaseni.html')));
 app.get('/profil', (req, res) => res.sendFile(path.join(__dirname, 'profil.html')));
 app.get('/chat', (req, res) => res.sendFile(path.join(__dirname, 'chat.html')));
 app.get('/koupit-mince', (req, res) => res.sendFile(path.join(__dirname, 'koupit mince.html')));
-app.get('/komentare', (req, res) => res.sendFile(path.join(__dirname, 'komentáře.html')));
+app.get('/komentare', (req, res) => res.sendFile(path.join(__dirname, 'komentare.html')));
 
 // Přihlášení
 app.post('/login', (req, res) => {
